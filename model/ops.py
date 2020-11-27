36886ed9e9d643881da9f342924c923a1294ce69
rebekah1124 <63224276+rebekah1124@users.noreply.github.com>
2020-11-28 05:40:09 +0700

    Update ops.py

--- model/ops.py
+++ model/ops.py
@@ -18,15 +18,16 @@ def create_subsequent_mask(target):
          [0, 0, 0, 0, 1],
          [0, 0, 0, 0, 0]]
     :param target: [batch size, target length]
-    :return:
+    
+    ## look ahead mask: 자기 자신보다 미래에 있는 단어들에 대한 마스킹 적용
     """
     batch_size, target_length = target.size()
 
-    # torch.triu returns the upper triangular part of a matrix based on user defined diagonal
+    # torch.triu: 상삼각행렬 with diagonal=사용자 지정
     subsequent_mask = torch.triu(torch.ones(target_length, target_length), diagonal=1).bool().to(device)
     # subsequent_mask = [target length, target length]
 
-    # repeat subsequent_mask 'batch size' times to cover all data instances in the batch
+    # subsequent_mask를 batch size만큼 반복 (batch에 있는 데이터를 다 다루기 위해)
     subsequent_mask = subsequent_mask.unsqueeze(0).repeat(batch_size, 1, 1)
     # subsequent_mask = [batch size, target length, target length]
 
@@ -40,14 +41,19 @@ def create_source_mask(source):
     masking tensor will be [False, False, False, False, False, True, True, True, False]
     :param source: [batch size, source length]
     :return: source mask
+    
+    #padding mask: 실질적인 의미를 갖지 않는 <pad> 토큰을 마스킹
+    boolean tensor에 대해서는 true를 가진 position들이 입력으로 취급하지 않음.
+    <pad>만 true
+    
     """
     source_length = source.shape[1]
 
-    # create boolean tensors which will be used to mask padding tokens of both source and target sentence
+    # <pad> 토큰을 마스킹할 boolean tensors 생성 (source & target 문장 모두에 대해서)
     source_mask = (source == pad_idx)
     # source_mask = [batch size, source length]
 
-    # repeat sentence masking tensors 'sentence length' times
+    # 문장길이만큼 반복
     source_mask = source_mask.unsqueeze(1).repeat(1, source_length, 1)
     # source_mask = [batch size, source length, source length]
 
@@ -72,11 +78,11 @@ def create_target_mask(source, target):
     target_mask = (target == pad_idx)
     # target_mask    = [batch size, target length]
 
-    # repeat sentence masking tensors 'sentence length' times
+    # 문장길이만큼 반복
     dec_enc_mask = source_mask.unsqueeze(1).repeat(1, target_length, 1)
     target_mask = target_mask.unsqueeze(1).repeat(1, target_length, 1)
 
-    # combine <pad> token masking tensor and subsequent masking tensor for decoder's self attention
+    # decoder의 self attention: padding mask O + look ahead mask O ==> 결합 (attention mask)
     target_mask = target_mask | subsequent_mask
     # target_mask = [batch size, target length, target length]
     return target_mask, dec_enc_mask
@@ -99,8 +105,11 @@ def create_position_vector(sentence):
 
 
 def create_positional_encoding(max_len, hidden_dim):
-    # PE(pos, 2i)     = sin(pos/10000 ** (2*i / hidden_dim))
-    # PE(pos, 2i + 1) = cos(pos/10000 ** (2*i / hidden_dim))
+    """
+    position encoding값 구하기: 단어의 상대적 위치에 대한 정보 제공
+    PE(pos, 2i)     = sin(pos/10000 ** (2*i / hidden_dim)) : 짝수
+    PE(pos, 2i + 1) = cos(pos/10000 ** (2*i / hidden_dim)) : 홀수 
+    """
     sinusoid_table = np.array([pos / np.power(10000, 2 * i / hidden_dim)
                                for pos in range(max_len) for i in range(hidden_dim)])
     # sinusoid_table = [max len * hidden dim]
@@ -108,10 +117,10 @@ def create_positional_encoding(max_len, hidden_dim):
     sinusoid_table = sinusoid_table.reshape(max_len, -1)
     # sinusoid_table = [max len, hidden dim]
 
-    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # calculate pe for even dimension
-    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # calculate pe for odd dimension
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # 짝수 차원
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # 홀수 차원
 
-    # convert numpy based sinusoid table to torch.tensor and repeat it 'batch size' times
+    # numpy에서 torch.tensor로 변환 후에 'batch size' 만큼 반복
     sinusoid_table = torch.FloatTensor(sinusoid_table).to(device)
     sinusoid_table[0] = 0.
 
@@ -119,6 +128,6 @@ def create_positional_encoding(max_len, hidden_dim):
 
 
 def init_weight(layer):
-    nn.init.xavier_uniform_(layer.weight)
+    nn.init.xavier_uniform_(layer.weight)  # Xavier Initialization 사용
     if layer.bias is not None:
         nn.init.constant_(layer.bias, 0)
