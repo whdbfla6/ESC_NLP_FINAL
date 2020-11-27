03286dedfda7d3325801d9ef9f1d8f3e056c8257
rebekah1124 <63224276+rebekah1124@users.noreply.github.com>
2020-11-28 00:56:32 +0700

    positionwise 주석추가

--- model/positionwise.py
+++ model/positionwise.py
@@ -20,16 +20,16 @@ class PositionWiseFeedForward(nn.Module):
         # permute x's indices to apply nn.Conv1d on input 'x'
         x = x.permute(0, 2, 1)                        # x = [batch size, hidden dim, sentence length]
         ''''''
-		f1 = xW1 + b1 (linear)
-		f2 = max(0,f1) (activation relu)
-		논문에 구체적으로 언급되어있지 않지만 transformer에 2개의 dropout이 더 추가됨.
-		Transformer has dropouts after ReLU in poisition-wise feed-forward networks (1) and after SoftMax in attentions (2)
-		''''''
+	f1 = xW1 + b1 (linear)
+	f2 = max(0,f1) (activation relu)
+	논문에 구체적으로 언급되어있지 않지만 transformer에 2개의 dropout이 더 추가됨.
+	Transformer has dropouts after ReLU in poisition-wise feed-forward networks (1) and after SoftMax in attentions (2)
+	''''''
         output = self.dropout(F.relu(self.conv1(x)))  # output = [batch size, feed forward dim, sentence length)
         output = self.conv2(output)                   # output = [batch size, hidden dim, sentence length)
         ''''''
-		f3 = f2W2 + b2  (linear)
-		''''''
+	f3 = f2W2 + b2  (linear)
+	''''''
         # permute again to restore output's original indices
         output = output.permute(0, 2, 1)              # output = [batch size, sentence length, hidden dim]
         return self.dropout(output)
